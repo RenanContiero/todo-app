420163447bce63e3a588eb247e9f188abe9c251c
lookfire <renancontiero@hotmail.com>
2020-04-06 17:46:11 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: string = 'Minha Tarefas';
+  public form: FormGroup; // importei
+
+  constructor() {
+    this.todos.push(new Todo(1,'Passear com o cachorro',true));
+    this.todos.push(new Todo(2,'Ir ao supermercado',false));
+    this.todos.push(new Todo(3,'Cortar o cabelo',false));
+    console.log(this.todos);
+ }
+
+ remove(todo: Todo){
+  const index = this.todos.indexOf(todo);
+  console.log('isso ' + index);
+  if(index !== -1){
+    this.todos.splice(index, 1); // 1 do parametro é a qdt de itens a ser removido
+  }
+ }
+
+ markAsDone(todo: Todo){
+   todo.done = true;
+ }
+
+ markAsUndone(todo: Todo){
+  todo.done = false;
+ }
+
 }
